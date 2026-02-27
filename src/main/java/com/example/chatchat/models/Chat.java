e5b450cea56708cbbd205113ddc088654c6f4292
EMamedov94 <44274561+EMamedov94@users.noreply.github.com>
2026-02-27 11:04:25 +0300

    Create Chat.java

---
+++ src/main/java/com/example/chatchat/models/Chat.java
@@ -0,0 +1,24 @@
+@Entity
+@Table(name = "chats")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "chat")
+    private List<ChatParticipant> participants;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
